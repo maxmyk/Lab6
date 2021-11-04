f4f4d001b5f79f0844d41bab9b6bdfc49fb32c3a
maxmyk <maksym.mykhasiuta@ucu.edu.ua>
2021-11-04 20:42:42 +0200

    results() added

--- target_game.py
+++ target_game.py
@@ -7,6 +7,7 @@ from typing import List
 Importing Lists
 """
 
+
 def generate_grid() -> List[List[str]]:
     """
     Generates list of lists of letters - i.e. grid for the game.
@@ -34,22 +35,23 @@ def get_words(file: str, letters: List[str]) -> List[str]:
             line = line.strip()
 
             if len(line) >= 4 and letters[4] in line:
-                    letters1 = []
-                    for i in range(len(line)):
-                        if line[i]!='-': letters1.append(line[i])
-                    letters1_occ = Counter(letters1)
-                    # letters = [letters[0][0],letters[0][1],letters[0][2],
-                    #            letters[1][0],letters[1][1],letters[1][2],
-                    #            letters[2][0],letters[2][1],letters[2][2]]
-                    letters_occ = Counter(letters)
-                    if letters1_occ - letters_occ == Counter():
-                        if len(line)%2 != 0:
-                            if line[len(line)//2] == letters[4]:
-                                words.add(line)
-                        else:
-                            if (line[len(line)//2] == letters[4]
-                            or line[len(line)//2-1] == letters[4]):
-                                words.add(line)
+                letters1 = []
+                for i in range(len(line)):
+                    if line[i] != '-':
+                        letters1.append(line[i])
+                letters1_occ = Counter(letters1)
+                # letters = [letters[0][0],letters[0][1],letters[0][2],
+                #            letters[1][0],letters[1][1],letters[1][2],
+                #            letters[2][0],letters[2][1],letters[2][2]]
+                letters_occ = Counter(letters)
+                if letters1_occ - letters_occ == Counter():
+                    if len(line) % 2 != 0:
+                        if line[len(line)//2] == letters[4]:
+                            words.add(line)
+                    else:
+                        if (line[len(line)//2] == letters[4]
+                                or line[len(line)//2-1] == letters[4]):
+                            words.add(line)
     return list(words)
 
 
@@ -77,22 +79,24 @@ def get_pure_user_words(user_words: List[str
     for word in user_words:
         for wordd in words_from_dict:
             if word != wordd:
-                if len(word)%2 != 0:
+                if len(word) % 2 != 0:
                     if word[len(word)//2] != letters[4]:
                         words.add(word)
                 else:
                     if (word[len(word)//2] != letters[4]
-                    or word[len(word)//2-1] != letters[4]):
+                            or word[len(word)//2-1] != letters[4]):
                         words.add(word)
     return list(words)
 
 
 def results():
-    url = "en"
     letters = generate_grid()
-    words_from_dict = get_words(url, letters)
+    letters = [letters[0][0], letters[0][1], letters[0][2],
+               letters[1][0], letters[1][1], letters[1][2],
+               letters[2][0], letters[2][1], letters[2][2]]
+    words_from_dict = get_words('en', letters)
     user_words = get_user_words()
-    print(words_from_dict)
-    print(user_words)
-    get_pure_user_words(user_words, letters, words_from_dict)
-    pass
+    filea = open("result.txt", "w", encoding='utf-8')
+    for line in get_pure_user_words(user_words, letters, words_from_dict):
+        filea.write(line+'\n')
+    filea.close()
