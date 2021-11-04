d8b8eb31fabf91815d890432423bf99a0f6d70d4
maxmyk <maksym.mykhasiuta@ucu.edu.ua>
2021-11-04 22:22:01 +0200

    final

--- target_game.py
+++ target_game.py
@@ -23,15 +23,13 @@ def generate_grid() -> List[List[str]]:
     print(grid)
     return grid
 
+
 def get_words(file: str, letters: List[str]) -> List[str]:
     """
     Reads the file f. Checks the words with rules and returns a list of words.
     """
     from collections import Counter
     words = set()
-    # letters = [letters[0][0],letters[0][1],letters[0][2],
-    # letters[1][0],letters[1][1],letters[1][2],
-    # letters[2][0],letters[2][1],letters[2][2]]
     with open(file, "r", encoding='utf-8') as file:
         for line in file:
             line = line.lower()
@@ -45,16 +43,9 @@ def get_words(file: str, letters: List[str]) -> List[str]:
                 letters1_occ = Counter(letters1)
                 letters_occ = Counter(letters)
                 if letters1_occ - letters_occ == Counter():
-                    if len(line) % 2 != 0:
-                        if line[len(line)//2] == letters[4]:
-                            words.add(line)
-                    else:
-                        if (line[len(line)//2] == letters[4]
-                                or line[len(line)//2-1] == letters[4]):
-                            words.add(line)
+                    words.add(line)
     return sorted(list(words))
-#get_words('en', [['w','u','m'],['r','o','v'],['k','i','f']])
-#get_words('en', [el for el in 'wumrovkif'])
+
 
 def get_user_words() -> List[str]:
     """
@@ -65,7 +56,6 @@ def get_user_words() -> List[str]:
     u_words = set()
     for line in stdin:
         u_words.add(line[:-1])
-    #print(sorted(u_words))
     return sorted(u_words)
 
 
@@ -79,17 +69,17 @@ def get_pure_user_words(user_words: List[str
     """
     words = set()
     wfd = set(words_from_dict)
+    from collections import Counter
     for word in user_words:
-        if word not in wfd and len(word) >= 4:
-            if len(word) % 2 != 0:
-                if word[len(word)//2] == letters[4]:
-                    words.add(word)
-            else:
-                if (word[len(word)//2] == letters[4]
-                        or word[len(word)//2-1] == letters[4]):
-                    words.add(word)
-            
-    #print(sorted(words))
+        if len(word) >= 4 and letters[4] in word and word not in wfd:
+            letters1 = []
+            for i in range(len(word)):
+                if word[i] != '-':
+                    letters1.append(word[i])
+            letters1_occ = Counter(letters1)
+            letters_occ = Counter(letters)
+            if letters1_occ - letters_occ == Counter():
+                words.add(word)
     return sorted(list(words))
 
 
