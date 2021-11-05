d23b3f343a91ad7c6d37766386714c1fe7d276ba
maxmyk <maksym.mykhasiuta@ucu.edu.ua>
2021-11-05 19:02:04 +0200

    Changed doctests and  fixed check_user_words

--- target_ua.py
+++ target_ua.py
@@ -22,36 +22,42 @@ def get_words(file, letters):
     Gets right words
     str, list() -> list()
     >>> get_words('base.lst', ['ь', 'и', 'є', 'ї', 'й'])
-    [('ирод', 'noun'), ('ирій', 'noun'), ('йняти', 'verb'), ('йог', 'noun'), \
-('йога', 'noun'), ('йод', 'noun'), ('йодат', 'noun'), ('йодид', 'noun'), \
-('йодил', 'noun'), ('йодит', 'noun'), ('йодль', 'noun'), ('йола', 'noun'), \
-('йолоп', 'noun'), ('йомен', 'noun'), ('йон', 'noun'), ('йорж', 'noun'), \
-('йот', 'noun'), ('йота', 'noun'), ('йти', 'verb'), ('йтися', 'verb'), \
-('євнух', 'noun'), ('єврей', 'noun'), ('євро', 'noun'), ('єгер', 'noun'), \
-('єдваб', 'noun'), ('єзуїт', 'noun'), ('єлей', 'noun'), ('ємний', 'adjective'), \
-('ємно', 'adverb'), ('єна', 'noun'), ('єнот', 'noun'), ('єпарх', 'noun'), \
-('єресь', 'noun'), ('єри', 'noun'), ('єрик', 'noun'), ('єство', 'noun'), \
-('єті', 'noun'), ('єхида', 'noun'), ('їда', 'noun'), ('їдало', 'noun'), \
-('їдати', 'verb'), ('їдець', 'noun'), ('їдиш', 'noun'), ('їдкий', 'adjective'), \
-('їдло', 'noun'), ('їдок', 'noun'), ('їдом', 'adverb'), ('їдуха', 'noun'), \
-('їдьма', 'adverb'), ('їжа', 'noun'), ('їжак', 'noun'), ('їзда', 'noun'), \
-('їздак', 'noun'), ('їздка', 'noun'), ('їздун', 'noun'), ('їнь', 'noun'), \
-('їсти', 'verb'), ('їстки', 'verb'), ('їхати', 'verb'), ('їхній', 'adjective')]
+    [('ирод', 'noun'), ('ирій', 'noun'), ('йняти', 'verb'), \
+('йог', 'noun'), ('йога', 'noun'), ('йод', 'noun'), \
+('йодат', 'noun'), ('йодид', 'noun'), ('йодил', 'noun'), \
+('йодит', 'noun'), ('йодль', 'noun'), ('йола', 'noun'), \
+('йолоп', 'noun'), ('йомен', 'noun'), ('йон', 'noun'), \
+('йорж', 'noun'), ('йорж', 'noun'), ('йот', 'noun'), \
+('йота', 'noun'), ('йти', 'verb'), ('йтися', 'verb'), \
+('євнух', 'noun'), ('єврей', 'noun'), ('євро', 'noun'), \
+('єгер', 'noun'), ('єдваб', 'noun'), ('єзуїт', 'noun'), \
+('єлей', 'noun'), ('ємний', 'adjective'), ('ємно', 'adverb'), \
+('єна', 'noun'), ('єнот', 'noun'), ('єпарх', 'noun'), \
+('єресь', 'noun'), ('єри', 'noun'), ('єрик', 'noun'), \
+('єрик', 'noun'), ('єство', 'noun'), ('єті', 'noun'), \
+('єхида', 'noun'), ('їда', 'noun'), ('їдало', 'noun'), \
+('їдати', 'verb'), ('їдець', 'noun'), ('їдиш', 'noun'), \
+('їдкий', 'adjective'), ('їдло', 'noun'), ('їдок', 'noun'), \
+('їдом', 'adverb'), ('їдуха', 'noun'), ('їдьма', 'adverb'), \
+('їжа', 'noun'), ('їжак', 'noun'), ('їзда', 'noun'), \
+('їздак', 'noun'), ('їздка', 'noun'), ('їздун', 'noun'), \
+('їнь', 'noun'), ('їсти', 'verb'), ('їстки', 'verb'), \
+('їхати', 'verb'), ('їхній', 'adjective')]
     """
-    ans = set()
+    ans = []
     with open(file, "r", encoding='utf-8') as file:
         for line in file:
             line = line.lower()
             line1 = line.split()
             if line1[0][0] in letters and len(line1[0]) <= 5:
                 if 'noun' in line1[1] or '/n' in line1[1]:
-                    ans.add((line1[0], 'noun'))
+                    ans.append((line1[0], 'noun'))
                 elif 'verb' in line1[1] or '/v' in line1[1]:
-                    ans.add((line1[0], 'verb'))
+                    ans.append((line1[0], 'verb'))
                 elif 'adj' in line1[1]:
-                    ans.add((line1[0], 'adjective'))
+                    ans.append((line1[0], 'adjective'))
                 elif 'adv' in line1[1]:
-                    ans.add((line1[0], 'adverb'))
+                    ans.append((line1[0], 'adverb'))
                 else:
                     pass
     return sorted(list(ans))
@@ -61,53 +67,37 @@ def check_user_words(user_words, language_part, letters, dict_of_words):
     """
     Checks if user words are proper and in dictionary
     list(), str, list(), list() -> list(), list()
-    >>> check_user_words(['ирод', 'ирій', 'йог', 'йога'], 'noun', \
-['ь', 'и', 'є', 'ї', 'й'], [('ирод', 'noun'), ('ирій', 'noun'), \
-('йняти', 'verb'), ('йог', 'noun'), ('йога', 'noun'), ('йод', 'noun'), \
-('йодат', 'noun'), ('йодид', 'noun'), ('йодил', 'noun'), \
-('йодит', 'noun'), ('йодль', 'noun'), ('йола', 'noun'), \
-('йолоп', 'noun'), ('йомен', 'noun'), ('йон', 'noun'), ('йорж', 'noun'), \
-('йот', 'noun'), ('йота', 'noun'), ('йти', 'verb'), ('йтися', 'verb'), \
-('євнух', 'noun'), ('єврей', 'noun'), ('євро', 'noun'), ('єгер', 'noun'), \
-('єдваб', 'noun'), ('єзуїт', 'noun'), ('єлей', 'noun'), ('ємний', 'adjective'), \
-('ємно', 'adverb'), ('єна', 'noun'), ('єнот', 'noun'), ('єпарх', 'noun'), \
-('єресь', 'noun'), ('єри', 'noun'), ('єрик', 'noun'), ('єство', 'noun'), \
-('єті', 'noun'), ('єхида', 'noun'), ('їда', 'noun'), ('їдало', 'noun'), \
-('їдати', 'verb'), ('їдець', 'noun'), ('їдиш', 'noun'), ('їдкий', 'adjective'), \
-('їдло', 'noun'), ('їдок', 'noun'), ('їдом', 'adverb'), ('їдуха', 'noun'), \
-('їдьма', 'adverb'), ('їжа', 'noun'), ('їжак', 'noun'), ('їзда', 'noun'), \
-('їздак', 'noun'), ('їздка', 'noun'), ('їздун', 'noun'), ('їнь', 'noun'), \
-('їсти', 'verb'), ('їстки', 'verb'), ('їхати', 'verb'), ('їхній', 'adjective')])
-    (['ирод', 'ирій', 'йог', 'йога'], \
-[('йняти', 'verb'), ('йод', 'noun'), ('йодат', 'noun'), \
-('йодид', 'noun'), ('йодил', 'noun'), ('йодит', 'noun'), \
-('йодль', 'noun'), ('йола', 'noun'), ('йолоп', 'noun'), \
-('йомен', 'noun'), ('йон', 'noun'), ('йорж', 'noun'), \
-('йот', 'noun'), ('йота', 'noun'), ('йти', 'verb'), \
-('йтися', 'verb'), ('євнух', 'noun'), ('єврей', 'noun'), \
-('євро', 'noun'), ('єгер', 'noun'), ('єдваб', 'noun'), \
-('єзуїт', 'noun'), ('єлей', 'noun'), ('ємний', 'adjective'), \
-('ємно', 'adverb'), ('єна', 'noun'), ('єнот', 'noun'), \
-('єпарх', 'noun'), ('єресь', 'noun'), ('єри', 'noun'), \
-('єрик', 'noun'), ('єство', 'noun'), ('єті', 'noun'), \
-('єхида', 'noun'), ('їда', 'noun'), ('їдало', 'noun'), \
-('їдати', 'verb'), ('їдець', 'noun'), ('їдиш', 'noun'), \
-('їдкий', 'adjective'), ('їдло', 'noun'), ('їдок', 'noun'), \
-('їдом', 'adverb'), ('їдуха', 'noun'), ('їдьма', 'adverb'), \
-('їжа', 'noun'), ('їжак', 'noun'), ('їзда', 'noun'), \
-('їздак', 'noun'), ('їздка', 'noun'), ('їздун', 'noun'), \
-('їнь', 'noun'), ('їсти', 'verb'), ('їстки', 'verb'), \
-('їхати', 'verb'), ('їхній', 'adjective')])
+    >>> check_user_words([], "verb", ['щ', 'ш', 'ь', 'у', 'ц'], get_words("base.lst", ['щ', 'ш', 'ь', 'у', 'ц']))
+    ([], ['убити', 'убути', 'увити', 'удати', 'ужити', 'узути', 'узяти', 'улити', 'умити', 'уміти', 'упити', 'урити', 'утяти', 'ухати', 'учити', 'учути', 'ушити', 'уїсти', 'шити'])
     """
     right = []
+    not_guessed = []
+    for elem in sorted(dict_of_words):
+        if elem[1] == language_part: 
+            not_guessed.append(elem[0])
     for word in user_words:
         if word[0] in letters and len(word[0]) <= 5:
             for elem in dict_of_words:
                 if elem[0] == word and elem[1] == language_part:
                     right.append(word)
-                    dict_of_words.remove(elem)
-    return sorted(right), sorted(dict_of_words)
+                    not_guessed.remove(elem[0])
+    return sorted(right), sorted(not_guessed)
+    # cor_use_words1 = []
+    # for i in range(len(user_words)):
+    #     if user_words[i][0] in letters:
+    #         cor_use_words1.append(user_words[i])
+    # cor_use_words2 = []
 
+    # for i in range(len(cor_use_words1)):
+    #     for j in range(len(dict_of_words)):
+    #         if cor_use_words1[i] == dict_of_words[j][0] and dict_of_words[j][1] == language_part:
+    #             cor_use_words2.append((cor_use_words1[i]))
+    # non_find_words = []
+    # for i in range(len(dict_of_words)):
+    #     if dict_of_words[i][1] == language_part and dict_of_words[i][0] not in cor_use_words2:
+    #         non_find_words.append(dict_of_words[i][0])
+
+    # return cor_use_words2, non_find_words
 
 if __name__ == "__main__":
     import doctest
